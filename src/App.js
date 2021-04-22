ae1088831421a383ed0fe39b16fc6e435f19b54a
kbrzdem <kbr_ozdm@hotmail.com>
2021-04-22 17:58:33 +0300

    düzenleme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom'
+import "./App.css";
+import List from './Components/list'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <main>
+        <Switch>
+            <Route path="/">
+              <List />
+            </Route>
+        </Switch>
+      </main>
+    </Router>
   );
+
 }
 
 export default App;
---
+++ src/Components/list.js
@@ -0,0 +1,90 @@
+
+import React, { useState, useEffect } from "react";
+
+
+const List = () => {
+    const [liste, setListe] = useState([]);
+    const [list, setList] = useState("");
+
+
+    const saveData = (newliste) => {
+        localStorage.setItem("liste", JSON.stringify(newliste));
+    };
+
+    useEffect(() => {
+        if (localStorage.getItem("liste")) {
+            setListe(JSON.parse(localStorage.getItem("liste")));
+        }
+    }, []);
+
+    const onAddTodo = () => {
+        if (list.trim()) {
+            const newliste = [...liste, { todo: list.trim(), id: Date.now() }];
+            setListe(newliste);
+            setList("");
+            saveData(newliste);
+        }
+    };
+    const deleteTodo = (id) => {
+        let newliste = liste.filter((todo) => todo.id !== id);
+        setListe(newliste);
+        saveData(newliste);
+    };
+
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-12 col-md-6 col-lg-12" >
+                <div className="Slist">
+             
+                <h1>Add Shopping List</h1>
+                    <table className="table">
+                        
+                        <thead>
+                            <tr>
+                                <th>
+                                    <input type="text" value={list} onChange={(e) => setList(e.target.value)} />
+                                </th>
+                                <th>
+                                    <button className="btn" onClick={onAddTodo}>
+                                        {" "}
+                  Add
+                </button>
+                                </th>
+                            </tr>
+                        </thead>
+
+                        <thead>
+                            <tr >
+                                <th className="mylist">My List </th>
+                            </tr>
+                        </thead>
+
+                        <tbody id="table" className="list" >
+                            {liste.map((todo) => (
+                                <tr key={todo.id}>
+                                    <td>{todo.todo}</td>
+                                    <td>
+                                        <button
+                                            className="btn btn-danger"
+                                            onClick={() => deleteTodo(todo.id)}
+                                        >
+                                            {" "}
+                    Delete{" "}
+                                        </button>{" "}
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div >
+            </div>
+        </div >
+           
+        </div>
+    );
+
+}
+
+export default List;
